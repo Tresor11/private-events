2ce4e29ef2c86f6be4820f7ceac3de175a3a1da7
Sharmarke <somaliwarsan@gmail.com>
2020-01-17 00:42:53 +0300

    fixed associations where user could not see list of attended events and events could not list attendees

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,8 +2,8 @@
 
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
-  has_many :attendees, through: :bookings, source: :event_attendee
   has_many :bookings, foreign_key: :booked_event_id
+  has_many :attendees, through: :bookings, source: :event_attendee
   validates :description, presence: true, length: { minimum: 5, maximum: 140 }
-  validates :date, presence: true
+# validates :date, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :created_events, foreign_key: 'creator_id', class_name: 'Event'
+  has_many :created_events, foreign_key: :creator_id, class_name: 'Event'
   has_many :bookings, foreign_key: :event_attendee_id
-  has_many :attended_events, through: :bookings
+  has_many :attended_events, through: :bookings, source: :booked_event
   validates :name, presence: true, length: { minimum: 3, maximum: 20 }
   valid_email = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: valid_email }, uniqueness: { case_sensitive: false }
