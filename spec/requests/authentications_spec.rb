8736e9642489482cdb263823161612039d5f1571
Tresor11 <tresor.moise2001@gmail.com>
2020-01-22 19:37:21 +0200

    the test context on the booking model

--- spec/requests/authentications_spec.rb
+++ spec/requests/authentications_spec.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Authentications", type: :request do
-  describe "GET /login" do
-    it "should show login page" do
+RSpec.describe 'Authentications', type: :request do
+  describe 'GET /login' do
+    it 'should show login page' do
       get login_path
       expect(response).to have_http_status(200)
     end
 
-    it "should redirect existing user to user show page" do
+    it 'should redirect existing user to user show page' do
       visit login_path
-      fill_in "id", with: 2
-      click_button "Login"
+      fill_in 'id', with: 2
+      click_button 'Login'
       current_path.should eq(user_path(2))
     end
   end
